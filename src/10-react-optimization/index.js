8e2691343ff74e7bba7d32bcaa2ea04102929348
Your Name <[bidurparajuli946@gmail.com]>
2021-08-24 11:34:02 +0545

    react optimization using usecallback

--- src/10-react-optimization/index.js
+++ src/10-react-optimization/index.js
@@ -1,28 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import {useFetch} from '../7-custom hook/2-useFetch'
 
 const url = 'https://reactcourseapi.gorakhjoshi.com/';
 
 const Index = () => {
    const  {products}= useFetch(url)
-   const [count, setCount]  = useState(0)
+    const [count, setCount] = useState(0)
+    const [cart, setCart]=useState(0)
+    console.log('Index component is running')
+
+    const addToCart = useCallback(() => {
+        
+        console.log('add to cart is running')
+       setCart(cart +1)
+    },[cart])
     return (
         <div>
             <h1>Count:{count} </h1>
-            <button onClick = {()=> setCount(count + 1)} className='btn'> Counter</button>
-            <Biglist products={products} />
+            <button onClick={() => setCount(count + 1)} className='btn'> Counter</button>
+            <h1> Cart:{cart}</h1>
+
+            <Biglist products={products} addToCart={addToCart}/>
             
         </div>
     )
 }
 
-const Biglist =React.memo( ({products}) => {
+const Biglist =React.memo( ({products,addToCart}) => {
     console.log('Big list is running')
     return(
         <div>
             {products.map((product)=> {
                 return (
-                    <Singleproducts key = {product.id} {...product} />
+                    <Singleproducts key = {product.id} {...product} addToCart={addToCart} />
                 )
             } )
             }
@@ -33,13 +43,14 @@ const Biglist =React.memo( ({products}) => {
 
 const Singleproducts = (props)=>{
     console.log('product is running')
-    const {fields } = props
+    const {fields,addToCart } = props
     const {price, name} =fields
     return(
         <article className ="product">
             <img src={fields.image[0].url } alt={name}></img>
         <h4>{name}</h4>
-        <p>{price}</p>
+            <p>{price}</p>
+            <button onClick={addToCart}>Add to Cart</button>
         </article>
     )
 }
